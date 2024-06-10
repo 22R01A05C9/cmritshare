c829aed0fd313fd48f0447342a777a87fcbb4fb6
vardhan ganugula <gvardhan2004@gmail.com>
2024-06-10 06:31:57 +0530

    upload frontend added

--- takeImage/a.py
+++ takeImage/a.py
@@ -1,30 +1,34 @@
-from flask import Flask, request, jsonify
-import base64
-import os
+from flask import Flask, request , jsonify, render_template
+from flask_cors import CORS
+import base64, os
+
 
 app = Flask(__name__)
-
-# Ensure the upload directory exists
+CORS(app)
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/upload', methods = ['POST'])
+def upload():
     data = request.get_json()
-    if 'image' not in data:
-        return jsonify({'error': 'No image data provided'}), 400
-    
-    image_data = data['image']
-    image_data = image_data.replace('data:image/png;base64,', '')
-    try:
-        image_bytes = base64.b64decode(image_data)
-        image_path = os.path.join(UPLOAD_FOLDER, 'uploaded_image.png')
-        with open(image_path, 'wb') as image_file:
-            image_file.write(image_bytes)
-        return jsonify({'message': 'Image uploaded successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    for image in data['imageList']:
+        imageData = image['imgUrl'].replace('data:image/png;base64,', '')
+        try:
+            imageByte = base64.b64decode(imageData)
+            imagePath = os.path.join('uploads', str(image['name']) + '.png')
+
+            with open(imagePath, 'wb') as imgFile:
+                imgFile.write(imageByte)
+        except Exception as e:
+            print('error' , e)
+    return jsonify({
+        "status" : "success"
+    })
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(host='0.0.0.0', port=5000)
